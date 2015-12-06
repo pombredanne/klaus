64a2e08a833e6ca3885d438208ab8d7c3c623a51
Jelmer Vernooij <jelmer@jelmer.uk>
2015-12-06 16:54:30 +0000

    Add manpage.

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
     ],
+    data_files=[('share/man/man8', ['klaus.8'])],
     install_requires=requires,
 )
 
